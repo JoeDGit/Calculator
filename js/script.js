715101917b1347a83329fdca189da753f9e3a477
JoeDGit <joe.dobbins89@gmail.com>
2020-05-02 17:17:58 +0100

    First draft of basic ui of calculator completed

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function divide (a, b) {
 function multiply (a, b) {	
 	return a * b;
 }
+
+function operate(x, y, z) {
+	if( y === '+' ) {
+		return add(x, z)
+	} else if( y === '-') {
+		return subtract(x, z)
+	} else if ( y === '/') {
+		return divide(x, z) }
+	else if ( y === '*') {
+		return multiply(x, z)
+	}
+	else return "invalid selection";
+};
+	
